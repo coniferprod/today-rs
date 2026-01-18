d77dd31bdb11fda86e73a0b13187e643efa4bb18
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2026-01-18 14:40:54 +0200

    Tweaked EventProvider interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,28 @@
 mod birthday;
 mod events;
+mod providers;
 
 use std::error::Error;
+use std::path::{Path, PathBuf};
 use chrono::{NaiveDate, Local, Datelike};
 use crate::events::{Event, Category, MonthDay};
+use crate::providers::{EventProvider, SimpleProvider};
+use crate::providers::textfile::TextFileProvider;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     birthday::handle_birthday();
 
-    let mut events = vec![
-        Event::new_singular(
-            NaiveDate::from_ymd_opt(2025, 12, 11).expect("valid date"),
-            String::from("Rust 1.92.0 released"),
-            Category::new("programming", "rust")),
+    let mut events: Vec<Event> = Vec::new();
 
-        Event::new_singular(
-            NaiveDate::from_ymd_opt(1996, 1, 23).expect("valid date"),
-            String::from("JDK 1.0 released"),
-            Category::new("programming", "java")),
-
-        Event::new_singular(
-            NaiveDate::from_ymd_opt(2008, 12, 3).expect("valid date"),
-            String::from("Python 3.0 released"),
-            Category::new("programming", "python")),
-            
-        Event::new_singular(
-            NaiveDate::from_ymd_opt(2015, 5, 15).expect("valid date"),
-            String::from("Rust 1.0.0 released"),
-            Category::new("programming", "rust")),
-
-        Event::new_singular(
-            NaiveDate::from_ymd_opt(2025, 9, 16).expect("valid date"),
-            String::from("Java 25 released"),
-            Category::new("programming", "java")),
-
-        Event::new_singular(
-            NaiveDate::from_ymd_opt(2025, 10, 7).expect("valid date"),
-            String::from("Python 3.14 released"),
-            Category::new("programming", "python")),
-
-            Event::new_singular(
-            NaiveDate::from_ymd_opt(2025, 12, 11).expect("valid date"),
-            String::from("Rust 1.92.0 released"),
-            Category::new("programming", "rust")),            
-    ];
+    let test_provider = SimpleProvider::new("test");
+    test_provider.get_events(&mut events);
+    
+    let text_file_provider = TextFileProvider::new(
+        "programming", Path::new("programming.txt"));
+    text_file_provider.get_events(&mut events);
 
     let today: NaiveDate = Local::now().date_naive();
     let today_month_day = MonthDay::new(today.month(), today.day());
-
-    let test_event = Event::new_singular(
-        today, 
-        String::from("Test event for today"), 
-        Category::from_primary("test")
-    );
-    events.push(test_event);
 
     for event in events {
         if today_month_day == event.month_day() {
---
+++ src/providers.rs
@@ -0,0 +1,121 @@
+use chrono::{NaiveDate, Local};
+use crate::{Event, Category};
+
+pub trait EventProvider {
+    fn name(&self) -> String;
+    fn get_events(&self, events: &mut Vec<Event>);
+}
+
+pub struct SimpleProvider {
+    name: String,
+}
+
+impl SimpleProvider {
+    pub fn new(name: &str) -> Self {
+        Self { name: name.to_string() }
+    }
+}
+
+impl EventProvider for SimpleProvider {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn get_events(&self, events: &mut Vec<Event>) {
+        let today: NaiveDate = Local::now().date_naive();
+
+        let test_event = Event::new_singular(
+            today, 
+            String::from("Test event for today"), 
+            Category::from_primary("test")
+        );
+        events.push(test_event);
+    }
+}
+
+pub mod textfile {
+    use std::path::{Path, PathBuf};
+    use std::fs::File;
+    use std::io::{BufReader, BufRead};
+    use std::fmt;
+
+    use chrono::NaiveDate;
+
+    use crate::EventProvider;
+    use crate::events::{Event, Category};
+
+    enum ReadingState {
+        Date,
+        Description,
+        Category,
+        Separator,
+    }
+
+    impl fmt::Display for ReadingState {
+        fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+            write!(f, "{}", match self {
+                ReadingState::Date => "DATE",
+                ReadingState::Description => "DESCRIPTION",
+                ReadingState::Category => "CATEGORY",
+                ReadingState::Separator => "SEPARATOR",
+            })
+        }
+    }
+
+    pub struct TextFileProvider {
+        name: String,
+        path: PathBuf,
+    }
+
+    impl TextFileProvider {
+        pub fn new(name: &str, path: &Path) -> Self {
+            Self { name: name.to_string(), path: path.to_path_buf() }
+        }
+    }
+
+    impl EventProvider for TextFileProvider {
+        fn name(&self) -> String {
+            self.name.clone()
+        }
+
+        fn get_events(&self, events: &mut Vec<Event>) {
+            let f = File::open(self.path.clone()).expect("path to text file");
+            let reader = BufReader::new(f);
+            let mut state = ReadingState::Date;
+            let mut date_string = String::new();
+            let mut description = String::new();
+            let mut category_string = String::new();
+
+            for line_result in reader.lines() {
+                let line = line_result.expect("read line");
+                match state {
+                    ReadingState::Date => {
+                        date_string = line;
+                        state = ReadingState::Description;
+                    },
+                    ReadingState::Description => {
+                        description = line;
+                        state = ReadingState::Category;
+                    },
+                    ReadingState::Category => {
+                        category_string = line;
+                        state = ReadingState::Separator;
+                    },
+                    ReadingState::Separator => {
+                        match NaiveDate::parse_from_str(&date_string, "%F") {
+                            Ok(date) => {
+                                let category = Category::from_str(&category_string);
+                                let event = Event::new_singular(date, description.clone(), category);
+                                events.push(event);
+                            },
+                            Err(_) => {
+                               eprintln!("Invalid timestamp '{}'", date_string);
+                            }
+                        }
+                        state = ReadingState::Date;
+                    },
+                }
+            }
+        }
+    }
+}
